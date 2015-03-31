7ebdc152218846fe8a0fbaf2c3d502531d3972e3
Avinash R <nashpapa@gmail.com>
2015-03-31 10:31:18 +0530

    use polyfill for String.prototype.endsWith for ES5 compatibility

--- gulp-crx-e-lance.js
+++ gulp-crx-e-lance.js
@@ -11,6 +11,20 @@ var crx= require('crx')
 
 var _crx= '.crx'
 
+// polyfill for String.prototype.endsWith (ES6)
+// see: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith#Polyfill
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(searchString, position) {
+		var subjectString = this.toString();
+		if (position === undefined || position > subjectString.length) {
+			position = subjectString.length;
+		}
+		position -= searchString.length;
+		var lastIndex = subjectString.indexOf(searchString, position);
+		return lastIndex !== -1 && lastIndex === position;
+	};
+}
+
 module.exports= (function pack(options) {
 	options= options || {}
 
